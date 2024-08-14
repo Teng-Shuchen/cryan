2996ff01e6f2df6db097695b08cc52279445f671
Cryan <1181317189@qq.com>
2024-08-14 23:16:08 +0800

    test

---
+++ src/config.js
@@ -0,0 +1,74 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: '你好 我叫滕述臣',
+    title: '一个走在AI路上的旅行者',
+  },
+  // page2部分
+  page2: {
+    // authorImg: 'page1.jpg',
+    authorImg: '微信头像.jpg',
+    xinhui: '我叫滕述臣，一名在读研究生。',
+    // qiuzhi: '',
+    guanyuwo: '熟练使用Java、Python语言以及Pytorch等深度学习框架；熟悉前后端开发框架；熟练掌握Transformer系列模型架构和原理。有着拥有丰富的政府端与企业端的项目经验，实习阶段参与过自动驾驶模型开发。' +
+        '擅长用AIGC加速个人生产效率。希望可以和大家一起学习，共同进步！' // 关于我
+  },
+
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '2022/9——2025/6', // 时间
+      desc: { // 经历
+        title: '河海大学-硕士',
+        list: ['发表核心论文《基于Transformer的局部时间步长二维水动力方法研究》', '拥有丰富的政府端与企业端的项目实战经验，曾独立开发过智慧化平台并已上线','自动驾驶大模型算法实习生'
+        ,'南瑞杯一等奖', '中国水科学数值模拟大赛三等奖']
+      }
+    }, {
+      date: '2018/9——2022/6',
+      desc: {
+        title: '河海大学-本科',
+        list: ['双修水文与水资源工程、计算机科学与技术', '参与江苏卫视"最强大脑"决赛测试','计算机二级','英语六级']
+      }
+    }
+    ]
+  },
+  // page5部分
+  page5: [{
+    title: '动物特写',
+    // content: '',
+    image: 'repli-bIEeTjMLCi.webp',
+    // href: ''
+  }, {
+    title: '尖端智能手表广告',
+    // content: '',
+    image: 'repli-S4EUxXFWDp.webp', //3
+    // href: ''
+  }, {
+    title: '摄影',
+    // content: '',
+    image: 'stablevideo_b3ed0754-c4a6-4976-a891-dc0069e61b77.png', //5
+    // href: ''
+  }, {
+    title: '街头拍摄',
+    // content: '',
+    image: 'repli-r5IiPhgBSM.webp',
+    // href: ''
+  }, {
+    title: '室内设计',
+    // content: '',
+    image: 'repli-wgY6jE2aIi.webp',
+    href: ''
+  }, {
+    title: '游戏场景设计',
+    // content: '', //6
+    image: 'repli-JY3DP35U1L.webp',
+    href: ''
+  }],
+  // page6部分
+  page6: {
+    weixin: "<img style='width:120px' src='../src/img/weixin.png' alt='加载失败'>",
+    qq: "<img style='width:120px' src='../src/img/qq.jpg' alt='加载失败'>",
+    test: "<img style='width:120px' src='../src/img/email.png' alt='加载失败'>"
+  }
+}
